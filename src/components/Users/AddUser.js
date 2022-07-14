5044ce92a1ca4945e37b0f5a50dd795fea345234
Zidane Innis <zidaneinnis@gmail.com>
2022-07-14 17:21:15 -0500

    Name and age successfully rendering on webpage

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,22 +9,41 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
   const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.valaue);
+    setEnteredUsername(event.target.value);
   };
   const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.valaue);
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
-        <label htmlFor="username">Age (Years) </label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age"> Age (Years) </label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
